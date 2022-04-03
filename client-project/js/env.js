43ebd5b041b3c5971991715ab1abe6f657a2548e
Rezoan <rlikhon@gmail.com>
2022-04-03 15:25:04 +0600

    initial commit

---
+++ client-project/js/env.js
@@ -0,0 +1,4 @@
+
+const LS_HOST = "push.lightstreamer.com";
+const LS_HTTP_PORT = "80";
+const LS_HTTPS_PORT = "443";
---
+++ env.js
@@ -0,0 +1,2 @@
+
+const POPUP_URL = "https://demos.lightstreamer.com/MPNStockListDemo/popup.html?item=";
